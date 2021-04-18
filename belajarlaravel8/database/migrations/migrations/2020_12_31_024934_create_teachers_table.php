b2ebddb4922685f5b4befe90ad7884066b982fb7
abuahmadsp <76844678+abuahmadsp@users.noreply.github.com>
2021-04-18 14:00:20 +0700

    Add files via upload

---
+++ belajarlaravel8/database/migrations/migrations/2020_12_31_024934_create_teachers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama', 200);
+            $table->char('nipy', 5)->unique();
+            $table->string('tugas', 200);
+            $table->string('mapel', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
